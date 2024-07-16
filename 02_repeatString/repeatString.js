5ccc3ea628f645cc91af62d4300364ee1661c5f3
Diano <Diano.faniry@gmail.com>
2024-07-16 16:58:35 +0400

    Reverse string done !

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
 
+const repeatString = function(stringToRepeat, numberOfRepetition) {
+    if(numberOfRepetition < 0){
+        return "ERROR";
+    }
+    let blankString = "";
+    for(let i = 0; i < numberOfRepetition; i++)
+    {
+        blankString += stringToRepeat;
+    }
+
+    return blankString
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(stringToReverse) {
+    let reversedStringArray = [];
 
+    let len = stringToReverse.length;
+    console.log("Len : " + len);
+
+    for (let i = len-1 ; i >= 0 ; i--)
+    {
+        reversedStringArray.push(stringToReverse[i]);
+    }
+
+    return reversedStringArray.join('');
 };
 
+//reverseString("diano");
 // Do not edit below this line
 module.exports = reverseString;
