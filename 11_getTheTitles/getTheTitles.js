07cc0291cf5999162843b52f874cf9c76550d46c
Diano <Diano.faniry@gmail.com>
2024-07-17 17:18:03 +0400

    getTheTitles done !

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+    {
+        title: 'Book',
+        author: 'Name'
+    },
+    {
+        title: 'Book2',
+        author: 'Name2'
+    }
+]
 
+const getTheTitles = function (books) {
+    let bookTitle = []
+    books.forEach(item => {
+        bookTitle.push(item.title);
+    });
+
+    return bookTitle;
 };
 
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
