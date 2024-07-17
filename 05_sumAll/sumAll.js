42288981846944db3f9d2ac1dd73a39ffc1b4da9
Diano <Diano.faniry@gmail.com>
2024-07-17 11:27:08 +0400

    sum All done !

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let total = 0
 
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+
+    if (a > b) {
+        let temp = 0;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        console.log("a :" + a + " b : " + b);
+
+    }
+    for (i = a; i <= b; i++) {
+        total += i;
+    }
+
+    return total;
 };
 
+console.log(sumAll(123, "1"));
 // Do not edit below this line
 module.exports = sumAll;
