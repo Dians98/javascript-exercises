613b66e9fb9153713b11774a572956130fed6bbe
Diano <Diano.faniry@gmail.com>
2024-07-17 11:50:42 +0400

    leap years done !

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
+
+    if (isDivisibleByHundred(year) && !isDivisibleByFourHundred(year)) {
+        return false;
+    }
+
+    if (isDivisibleByFour(year)) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+function isDivisibleByFour(year) {
+    return year % 4 == 0 ? true : false;
+}
+
+function isDivisibleByHundred(year) {
+    return year % 100 == 0 ? true : false;
+}
+
+function isDivisibleByFourHundred(year) {
+    return year % 400 == 0 ? true : false;
+}
+
+
+console.log(leapYears(1800));
 
 // Do not edit below this line
 module.exports = leapYears;
