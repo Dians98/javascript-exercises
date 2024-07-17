42bc86f693fd7bb04d52e367ca1d0f1ceece09a3
Diano <Diano.faniry@gmail.com>
2024-07-17 16:04:01 +0400

    fibonnaci done !

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
 
+    let fibSeq = [0, 1];
+
+    if (number == 0 || number == 1) {
+        return fibSeq[number];
+    }
+    else if (number < 0) {
+        return "OOPS";
+    };
+
+    if (typeof (number) == "string" || typeof (number) == "String") {
+        number = parseInt(number);
+    }
+
+    for (i = 2; i <= number; i++) {
+        let FibSeqNew = fibSeq[i - 2] + fibSeq[i - 1];
+        fibSeq.push(FibSeqNew);
+    }
+
+    return fibSeq[number];
 };
+
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
