da4ec2c81942673b9348a2174a9f0d037c6e3f4b
Diano <Diano.faniry@gmail.com>
2024-07-17 11:04:02 +0400

    Remove from array done !

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let len = array.length;
+
+    array = removeDuplicateData(array);
+
+    args.forEach(arg => {
+        if (array.includes(arg)) {
+            let indexOf = array.indexOf(arg);
+            array.splice(indexOf, 1);
+        }
+    });
+    return array;
 };
 
+//Supprime les doublons d'abord
+function removeDuplicateData(data){
+    return data.filter((value, index) => data.indexOf(value) === index);
+}
+
+
+//removeFromArray([1, 2, 2, 3], 2);
 // Do not edit below this line
 module.exports = removeFromArray;
